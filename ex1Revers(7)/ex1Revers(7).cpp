296ae4fe139d1fddd6d69e96fa1dfe532832e783
Lumaks aic <lumaksaic@gmail.com>
2023-04-15 02:42:58 +0300

    Добавьте файлы проекта.

---
+++ ex1Revers(7)/ex1Revers(7).cpp
@@ -0,0 +1,30 @@
+﻿// ex1Revers(7).cpp 
+#include <iostream>
+using namespace std;
+
+void reversit(wchar_t[]);      // prototype function
+
+int main()
+{
+    const int MAX = 80;         // max length string
+    wchar_t s[MAX];
+
+    wcout << "Enter string:\n";
+    wcin.get(s, MAX);           // we get a line with spaces
+
+    reversit(s);                // revers string
+
+    wcout << "Result reversing: " << s << endl;
+
+    return 0;
+}
+
+void reversit(wchar_t str[])
+{
+    for (int a = 0; a < wcslen(str) / 2; a++)
+    {
+        char temp = str[a];
+        str[a] = str[wcslen(str) - a - 1];
+        str[wcslen(str) - a - 1] = temp;
+    }
+}
